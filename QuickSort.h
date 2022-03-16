9c5a0c682e3e2149035110374c78b3160ea0b677
UIT-21521845 <96047328+LTBach@users.noreply.github.com>
2022-03-16 13:22:15 +0700

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int partition(int arr[],int low,int high)
+int partition(double arr[],int low,int high)
 {
-    int pivot=arr[high];
+    double pivot=arr[high];
     int i=(low - 1);
  
     for (int j=low;j<=high-1;j++){
@@ -15,11 +14,15 @@ int partition(int arr[],int low,int high)
     swap(arr[i + 1], arr[high]);
     return (i + 1);
 }
-void quickSort(int arr[],int low,int high)   //quickSort(a,0,n-1);
+void QuickSort(double arr[],int low,int high)   //quickSort(a,0,n-1);
 {
     if (low<high){
         int pi=partition(arr, low, high);
-        quickSort(arr,low,pi-1);
-        quickSort(arr,pi+1,high);
+        QuickSort(arr,low,pi-1);
+        QuickSort(arr,pi+1,high);
     }
+}
+void QuickSort(double a[], int n)
+{
+    QuickSort(a,0,n-1);
 }
